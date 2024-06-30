4f709ae3172c77879194dd99917e149c37f51df7
Alexey Konovalov <asknvl@gmail.com>
2024-06-30 22:01:18 +0300

    init

---
+++ ILogger.cs
@@ -0,0 +1,12 @@
+﻿namespace logger
+{
+    public interface ILogger
+    {
+        void dbg(string tag, string text);
+        void warn(string tag, string text);
+        void err(string tag, string text);
+        void inf(string tag, string text);
+        void inf_urgent(string tag, string text);
+    }
+    
+}
